8467eedc05e92de8c0dc7bd253bc60f681dbee3b
thewizard003 <thewizard003@gmail.com>
2022-03-18 20:18:47 +0200

    made the buttons play the music once

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, MouseEventHandler } from 'react';
 import LoopButton from './LoopButton';
 import './App.css';
-import {startPlaying,stopPlaying} from './mainLoopHandler'
 
 const App: React.FC = () => {
+
+  const [mainLoopPlaying,setMainLoopPlaying] = useState(false)
+
+  const toggle: MouseEventHandler = (event:React.MouseEvent<HTMLButtonElement>) => {
+    setMainLoopPlaying(!mainLoopPlaying);
+  }
 
   const [loopItems]  = useState(Array.from(Array(9)).map((e,i)=>i+1).map((id)=>{
     return { id:id, musicFile:(id).toString()+".mp3"}
@@ -13,14 +18,11 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <div>
-          <button className="main-buttons" onClick={startPlaying}>
-            Play
-          </button>
-          <button className="main-buttons" onClick={stopPlaying}>
-            Stop
+          <button className="main-buttons" onClick={toggle}>
+            {mainLoopPlaying?"Stop":"Play"}
           </button>
         </div>
-        <div>
+        <div className='loop-buttons-container'>
           {loopItems.map((item)=><LoopButton key={item.id} item={item}/>)}
         </div>
       </header>
--- src/LoopButton.tsx
+++ src/LoopButton.tsx
@@ -1,18 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+const song1 = require("./static/music/1.mp3")
+const song2 = require("./static/music/2.mp3")
+const song3 = require("./static/music/3.mp3")
+const song4 = require("./static/music/4.mp3")
+const song5 = require("./static/music/5.mp3")
+const song6 = require("./static/music/6.mp3")
+const song7 = require("./static/music/7.mp3")
+const song8 = require("./static/music/8.mp3")
+const song9 = require("./static/music/9.mp3")
+
+
 interface LoopItem{
     id: number
     musicFile: string
 }
-type Props = {
+type LoopButtonProps = {
     item: LoopItem
 }
 
-const LoopButton:React.FC<Props> = ({item}) => {
+const LoopButton:React.FC<LoopButtonProps> = ({item}) => {
+
+    const [audio,setAudio] = useState<HTMLAudioElement>(new Audio(song1));
+
+    const [playing, setPlaying] = useState<boolean>(false);
     
+    const toggle = () => setPlaying(!playing);
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+        },
+        [playing]
+    );
+    
+    useEffect(() => {
+        switch(item.musicFile){
+            case "1.mp3":{
+                setAudio(new Audio(song1))
+                break
+            }
+            case "2.mp3":{
+                setAudio(new Audio(song2))
+                break
+            }
+            case "3.mp3":{
+                setAudio(new Audio(song3))
+                break
+            }
+            case "4.mp3":{
+                setAudio(new Audio(song4))
+                break
+            }
+            case "5.mp3":{
+                setAudio(new Audio(song5))
+                break
+            }
+            case "6.mp3":{
+                setAudio(new Audio(song6))
+                break
+            }
+            case "7.mp3":{
+                setAudio(new Audio(song7))
+                break
+            }
+            case "8.mp3":{
+                setAudio(new Audio(song8))
+                break
+            }
+            case "9.mp3":{
+                setAudio(new Audio(song9))
+                break
+            }
+        }
+        
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+        audio.removeEventListener('ended', () => setPlaying(false));
+        };
+    }, []);
     return (
-        <button className="loop-button">
-  
-        </button>
+            <button className="loop-button" onClick={()=>{toggle()}}>{item.id}</button>
       );
 }
   
